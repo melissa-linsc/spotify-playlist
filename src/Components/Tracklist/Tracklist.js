8c4174ba8114eb4776fe8ac82e8e8e3ded7baf4f
melissa-linsc <118235468+melissa-linsc@users.noreply.github.com>
2024-03-01 16:34:22 +0000

    Finished Components

---
+++ src/Components/Tracklist/Tracklist.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import styles from './Tracklist.module.css'
+
+import Track from '../Track/Track'
+
+function Tracklist (props) {
+    return (
+        <div className={styles.Tracklist}>
+        {/* <!-- You will add a map method that renders a set of Track components  --> */}
+        {props.userSearchResults.map((track) => (
+          <Track track={track} key={track.id} onAdd={props.onAdd} isRemoval={props.isRemoval} onRemove={props.onRemove} />
+        ))}
+      </div>
+    );
+}
+
+export default Tracklist;
